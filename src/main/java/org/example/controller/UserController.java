858ced41864e48af502627147b0fbc0cc7b9466a
Roma <zhogol.roma1999@gmail.com>
2025-09-27 15:21:54 +0300

    add: Hateoas to UserController

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -5,10 +5,15 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.dtos.UserDTO;
 import org.example.service.UserService;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/api/users")
@@ -23,28 +28,52 @@ public class UserController {
 
     @GetMapping
     @Operation(summary = "Получить список всех пользователей")
-    public List<UserDTO> getAllUsers() {
-        return userService.getAllUsers();
+    public CollectionModel<EntityModel<UserDTO>> getAllUsers() {
+        List<EntityModel<UserDTO>> users = userService.getAllUsers().stream()
+                .map(user -> EntityModel.of(user,
+                        linkTo(methodOn(UserController.class).getUser(user.getId())).withSelfRel(),
+                        linkTo(methodOn(UserController.class).getAllUsers()).withRel("all-users")
+                ))
+                .toList();
+
+        return CollectionModel.of(users,
+                linkTo(methodOn(UserController.class).getAllUsers()).withSelfRel(),
+                linkTo(methodOn(UserController.class).createUser(null)).withRel("create")
+        );
     }
 
     @Operation(summary = "Получить пользователя по ID")
     @GetMapping("/{id}")
-    public UserDTO getUser(@Parameter(description = "ID пользователя") @PathVariable Long id) {
-        return userService.getUser(id);
+    public EntityModel getUser(@Parameter(description = "ID пользователя") @PathVariable Long id) {
+        UserDTO user = userService.getUser(id);
+
+        return EntityModel.of(user, linkTo(methodOn(UserController.class).getUser(id)).withSelfRel(),
+                            linkTo(methodOn(UserController.class).getAllUsers()).withRel("allUsers"),
+                            linkTo(methodOn(UserController.class).deleteUser(id)).withRel("delete"),
+                            linkTo(methodOn(UserController.class).updateUser(id, user)).withRel("update")
+        );
     }
 
     @PostMapping
     @Operation(summary = "Создать нового пользователя")
-    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO dto) {
-        return ResponseEntity.ok(userService.createUser(dto));
+    public EntityModel<UserDTO> createUser(@RequestBody UserDTO userDTO) {
+        UserDTO user = userService.createUser(userDTO);
+        return EntityModel.of(user,
+                linkTo(methodOn(UserController.class).getUser(user.getId())).withSelfRel(),
+                linkTo(methodOn(UserController.class).getAllUsers()).withRel("allUsers")
+        );
     }
 
     @PutMapping("/{id}")
     @Operation(summary = "Обновить пользователя")
-    public ResponseEntity<UserDTO> updateUser(
+    public EntityModel<UserDTO> updateUser(
             @PathVariable Long id,
             @RequestBody UserDTO dto) {
-        return ResponseEntity.ok(userService.updateUser(id, dto));
+        UserDTO updatedUser = userService.updateUser(id, dto);
+        return EntityModel.of(updatedUser,
+                                linkTo(methodOn(UserController.class).getUser(updatedUser.getId())).withSelfRel(),
+                                linkTo(methodOn(UserController.class).getAllUsers()).withRel("allUsers")
+        );
     }
 
     @DeleteMapping("/{id}")
