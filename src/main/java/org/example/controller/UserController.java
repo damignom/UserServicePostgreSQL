5da7ed0c99297d234d85b24bd3631e3a5bcc0322
Roma <zhogol.roma1999@gmail.com>
2025-09-27 14:33:34 +0300

    add: Swagger annotations to UserController

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -1,5 +1,8 @@
 package org.example.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.dtos.UserDTO;
 import org.example.service.UserService;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@Tag(name = "User API", description = "Операции с пользователями")
 public class UserController {
 
     private final UserService userService;
@@ -18,21 +22,25 @@ public class UserController {
     }
 
     @GetMapping
+    @Operation(summary = "Получить список всех пользователей")
     public List<UserDTO> getAllUsers() {
         return userService.getAllUsers();
     }
 
+    @Operation(summary = "Получить пользователя по ID")
     @GetMapping("/{id}")
-    public UserDTO getUser(@PathVariable Long id) {
+    public UserDTO getUser(@Parameter(description = "ID пользователя") @PathVariable Long id) {
         return userService.getUser(id);
     }
 
     @PostMapping
+    @Operation(summary = "Создать нового пользователя")
     public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO dto) {
         return ResponseEntity.ok(userService.createUser(dto));
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Обновить пользователя")
     public ResponseEntity<UserDTO> updateUser(
             @PathVariable Long id,
             @RequestBody UserDTO dto) {
@@ -40,7 +48,8 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+    @Operation(summary = "Удалить пользователя по ID")
+    public ResponseEntity<Void> deleteUser(@Parameter(description = "ID пользователя")@PathVariable Long id) {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
